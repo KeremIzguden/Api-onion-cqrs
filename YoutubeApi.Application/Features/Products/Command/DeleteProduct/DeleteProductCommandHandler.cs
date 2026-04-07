7701e9581ccb853422fc75504ed9525bb66cb97d
keremizguden <keremizguden@gmail.com>
2026-04-07 14:46:37 +0300

    register tamamlandı

--- YoutubeApi.Application/Features/Products/Command/DeleteProduct/DeleteProductCommandHandler.cs
+++ YoutubeApi.Application/Features/Products/Command/DeleteProduct/DeleteProductCommandHandler.cs
@@ -1,25 +1,29 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YoutubeApi.Application.Bases;
 using YoutubeApi.Application.Interfaces.UnitOfWorks;
 using YoutubeApi.Domain.Entities;
 
 namespace YoutubeApi.Application.Features.Products.Command.DeleteProduct
 {
-    public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommandRequest,Unit>
+    public class DeleteProductCommandHandler : BaseHandler, IRequestHandler<DeleteProductCommandRequest,Unit>
     {
-        private readonly IUnitOfWork unitOfWork;    
-        public DeleteProductCommandHandler(IUnitOfWork unitOfWork)
+            
+        public DeleteProductCommandHandler(IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
         {
-            this.unitOfWork = unitOfWork;
+            
         }
         public async Task<Unit> Handle(DeleteProductCommandRequest request, CancellationToken cancellationToken)
         {
             var product = unitOfWork.GetReadRepository<Product>().GetAsync(x => x.Id == request.Id && !x.IsDeleted);
-            product.IsDeleted = false;
+            product.IsDeleted = true;
+
             await unitOfWork.GetWriteRepository<Product>().UpdateAsync(product);    
             await unitOfWork.SaveAsync();
 
